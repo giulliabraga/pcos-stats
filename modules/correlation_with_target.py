7633b24a7fb4bd2e480cdbba332df280afd2a3de
Giullia Braga <giubdam@gmail.com>
2024-11-27 13:01:07 -0300

    Minor updates

--- modules/correlation_with_target.py
+++
@@ -1,6 +0,0 @@
-def correlation_with_target(dataset, target):
-    '''
-    Pearson correlation coefficient for each variable with respect to the target
-    '''
-    correlation_with_target = dataset.corr()[target].abs().sort_values(ascending=False)
-    print(correlation_with_target)
